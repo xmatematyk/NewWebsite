4bc5c8f56b9421f802df76a393610effe9b99d6e
cysk <feskyykontakt@gmail.com>
2024-02-02 21:35:34 +0100

    t

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -9,7 +9,7 @@ const Aboutme = () => {
         <p>
           Siema! Jestem Olivier (aka. Feskyy, cysk), i mam 16 lat. Jestem na drugim roku technikum o profilu techniczno-programistycznym. 
           <p></p>Moje główne zainteresowania koncentrują się wokół tworzenia botów na komunikator Discord i tworzenia stron internetowych.
-          Gram również w gry takie jak Valorant, Fortnite czy nawet minecraft, przez całe dziecinstwo grałem w minecrafta na community blazingpack'a,
+          Gram również w gry takie jak Valorant, Fortnite czy nawet minecraft, przez całe dziecinstwo grałem w minecraft&aposa na community blazingpack&aposa,
           przez co jestem dosyc toksyczną osobą. Odkąd poznałem swiat tworzenia botów Discord wciągnałem sie w tworzenie własnych botów, które są  wstanie
           zautoamtyzowac cały serwer. Od jakiegos czasu uczę się tworzyc grafiki - głownie loga i grafiki dla botów do embeda.
         </p>

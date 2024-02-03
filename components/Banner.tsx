9803add0769622ae23e6ec527be28f174ac72892
cysk <feskyykontakt@gmail.com>
2024-02-03 01:21:24 +0100

    got

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,11 +5,11 @@ import ProfileImg from "../public/img/profileImg.jpg";
 
 const Banner = () => {
   return (
-    <div className="wrapper flex flex-col md:flex-row items-center gap-10">
+    <div className="wrapper flex  flex-col md:flex-row items-center gap-10">
       {/* Image */}
-      <div className="w-44 h-44 rounded-full bg-black">
+      <div className="w-44 h-44 md:top-[-1.5rem] relative rounded-full bg-black">
         <Image
-          className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover"
+          className="w-full h-full rounded-full  border-[1px] border-indigo-700 p-2 object-cover"
           src={ProfileImg}
           alt="ProfileImg"
         />
@@ -22,10 +22,11 @@ const Banner = () => {
         </h3>
         <p className="text-base tracking-wide text-center md:text-left">
           Czesc! jestem Olivier, oraz mam 16 lat. W wolnym czasie lubie pograc w gry komputerowe oraz programowac, jednak te programowanie zdobyło moje serce. Programuje od 2021 (3 lata) i nadal sprawia mi to dużo zabawy.
-        </p>
-        <div className="top-5 relative">
+          <div className="top-[2rem] relative">
         <img src="https://discord.c99.nl/widget/theme-4/551123399834075137.png" alt="" />
       </div>
+        </p>
+        
       </div>
       
     </div>
